705adb2a9e6564f623837f76b37bf580bfe5bdf2
Natalie Agus <nn88blue@gmail.com>
2023-07-03 22:25:41 +0800

    feat: add export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+const repo = "nextra-demo";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
+module.exports = {
+  ...withNextra(),
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
