e5b5dd6c6a4cfe18b7d1337df94eefb2c09e30ec
Natalie Agus <nn88blue@gmail.com>
2023-07-04 10:34:57 +0800

    feat: initial commit

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  staticImage: true,
+  latex: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true
+})
+
+export default withNextra({
+  reactStrictMode: true,
+  eslint: {
+    // Eslint behaves weirdly in this monorepo.
+    ignoreDuringBuilds: true
+  },
+  redirects: () => [
+    {
+      source: '/docs/docs-theme/built-ins/callout',
+      destination: '/docs/guide/built-ins/callout',
+      permanent: true
+    }
+  ]
+})
